e1e3812a95a26227eaf3f8f36e25a970ac4a2912
bendrucker11 <93153248+bendrucker11@users.noreply.github.com>
2021-12-29 12:09:25 -0500

    Create handRank.py

---
+++ handRank.py
@@ -0,0 +1,12 @@
+class HandRank:
+    ranks = {
+        0: "None",
+        1: "Pair",
+        2: "Two Pair",
+        3: "Three of a Kind",
+        4: "Straight",
+        5: "Flush",
+        6: "Full House",
+        7: "Four of a Kind",
+        8: "Straight Flush",
+    }
