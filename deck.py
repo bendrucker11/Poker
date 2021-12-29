0aaea8a15169d833908e3d528f62d7326cd52c8f
bendrucker11 <93153248+bendrucker11@users.noreply.github.com>
2021-12-29 12:07:06 -0500

    Update deck.py

--- deck.py
+++ deck.py
@@ -1,13 +1,12 @@
-import numpy as np
 import random
 from card import Card
 
 
 class Deck:
-    cards = np.empty(52)  # initialize deck to size 52
+    cards = []  # initialize deck to size 52
 
     def __init__(self):
-        for c in range(self.cards.size):
+        for c in range(52):
             if c < 13:
                 self.cards[c] = Card(c + 1, 0)
             elif c < 26:
@@ -29,22 +28,20 @@ class Deck:
         return card.suit
 
     def get_num_cards(self):
-        return self.cards.size
+        return len(self.cards)
 
     def shuffle(self):
         temp = self.cards
-        shuffled = np.empty(52)
+        shuffled = []
+
         for i in range(52):
             shuffled[i] = random.choice(temp)
             temp.remove(shuffled[i])
-
+        self.cards = shuffled
 
     def deal(self, num_cards):
-        deal = np.empty(num_cards, dtype=object)
-        for c in range(num_cards):
-            deal[c] = self.cards[c]
-        smaller = np.empty(52 - num_cards)
-        for c in range(smaller.size):
-            smaller[c] = self.cards[num_cards + c]
-        self.cards = smaller
-        return deal
+        dealt = []
+        for i in range(num_cards):
+            dealt[i] = self.cards[i]
+            self.cards.remove(dealt[i])
+        return dealt
